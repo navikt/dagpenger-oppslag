2b6fce113b33064101d61b011da6a07c231b6914
Nico Kuijlen <nico.kuijlen@nav.no>
2018-11-05 12:36:00 +0100

    Add endpoint for fetching saker from Arena

--- src/main/kotlin/no/nav/dagpenger/oppslag/arena/ArenaController.kt
+++ src/main/kotlin/no/nav/dagpenger/oppslag/arena/ArenaController.kt
@@ -1,28 +1,45 @@
 package no.nav.dagpenger.oppslag.arena
 
 import io.ktor.application.call
+import io.ktor.http.HttpStatusCode
 import io.ktor.request.receive
+import io.ktor.response.respond
 import io.ktor.response.respondText
 import io.ktor.routing.Routing
-import io.ktor.routing.get
 import io.ktor.routing.post
+import no.nav.arena.services.lib.sakvedtak.SaksInfo
+import no.nav.arena.services.sakvedtakservice.FaultFeilIInputMsg
 
 fun Routing.arena(arenaClient: ArenaClientSoap) {
-    post("arena/sak") {
-        val (behandlendeEnhetId, fødselsnummer ) = call.receive<BestillArenaSakRequest>()
+    post("arena/opprettsak") {
+        val (behandlendeEnhetId, fødselsnummer) = call.receive<BestillArenaSakRequest>()
 
         val arenaSakId = arenaClient.bestillOppgave(behandlendeEnhetId, fødselsnummer)
 
         call.respondText(arenaSakId)
     }
 
-    post("arena/hentsak") {
-        val (behandlendeEnhetId, fødselsnummer ) = call.receive<BestillArenaSakRequest>()
+    post("arena/finnsak") {
+        val fødselsnummer = call.receive<String>()
 
-        val arenaSakId = arenaClient.hentDagpengerSaker(fødselsnummer, "PERSON")
+        try {
+            val saker = arenaClient.getDagpengerSaker(fødselsnummer, "PERSON", false)
 
-        call.respondText("awe")
+            val newestActiveSak = findNewestActiveSak(saker)
+
+            if (newestActiveSak == null) {
+                call.respond(HttpStatusCode.NotFound)
+            } else {
+                call.respond(newestActiveSak.saksId)
+            }
+        } catch (inputException : FaultFeilIInputMsg) {
+            call.respond(HttpStatusCode.BadRequest, inputException.faultInfo)
+        }
     }
+}
+
+fun findNewestActiveSak(saker : List<SaksInfo>) : SaksInfo? {
+    return saker.filter { it.sakstatus == "AKTIV" }.maxBy { it.sakOpprettet.toGregorianCalendar() }
 }
 
 data class BestillArenaSakRequest(
