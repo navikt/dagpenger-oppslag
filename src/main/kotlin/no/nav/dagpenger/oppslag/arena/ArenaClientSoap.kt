f15b6f184c9de4659c8c4803a2273a454d243abd
Nico Kuijlen <nico.kuijlen@nav.no>
2018-11-06 14:04:10 +0100

    Do not set 'frist' when creating sak (needs to be tested)

--- src/main/kotlin/no/nav/dagpenger/oppslag/arena/ArenaClientSoap.kt
+++ src/main/kotlin/no/nav/dagpenger/oppslag/arena/ArenaClientSoap.kt
@@ -12,9 +12,6 @@ import no.nav.tjeneste.virksomhet.behandlearbeidogaktivitetoppgave.v1.informasjo
 import no.nav.tjeneste.virksomhet.behandlearbeidogaktivitetoppgave.v1.informasjon.Tema
 import no.nav.tjeneste.virksomhet.behandlearbeidogaktivitetoppgave.v1.meldinger.BestillOppgaveRequest
 import no.nav.tjeneste.virksomhet.behandlearbeidogaktivitetoppgave.v1.meldinger.BestillOppgaveResponse
-import java.util.Calendar
-import java.util.GregorianCalendar
-import javax.xml.datatype.DatatypeFactory
 import javax.xml.ws.Holder
 
 class ArenaClientSoap(val oppgaveV1: BehandleArbeidOgAktivitetOppgaveV1, val hentsak: SakVedtakPortType) {
@@ -28,9 +25,8 @@ class ArenaClientSoap(val oppgaveV1: BehandleArbeidOgAktivitetOppgaveV1, val hen
             prioritet = Prioritet().apply { value = "HOY" }
             bruker = Person().apply { ident = fødselsnummer }
             this.behandlendeEnhetId = behandlendeEnhetId
-            //TODO: find out what to set as deadline/frist
-            frist = GregorianCalendar().also { it.add(Calendar.DATE, 1) }
-                .let { DatatypeFactory.newInstance().newXMLGregorianCalendar(it) }
+            // TODO: verify if frist is optional/has default value when creating sak in arena
+            // frist = GregorianCalendar().let { DatatypeFactory.newInstance().newXMLGregorianCalendar(it) }
         }
 
         val response: BestillOppgaveResponse = oppgaveV1.bestillOppgave(request)
