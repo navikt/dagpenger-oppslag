83fd26b6ed98396c337abf42b2541dfbdf1853f7
Atle Nordland <atle.nordland@nav.no>
2018-12-05 10:34:00 +0100

    Temporary fix: Ignore BehandleInngaaendeJournal

--- src/main/kotlin/no/nav/dagpenger/oppslag/Environment.kt
+++ src/main/kotlin/no/nav/dagpenger/oppslag/Environment.kt
@@ -7,8 +7,10 @@ data class Environment(
     val dagpengerPersonUrl: String = getEnvVar("VIRKSOMHET_PERSON_V3_ENDPOINTURL"),
     val dagpengerArbeidsfordelingUrl: String = getEnvVar("VIRKSOMHET_ARBEIDSFORDELING_V1_ENDPOINTURL"),
     val dagpengerArenaOppgaveUrl: String = getEnvVar("VIRKSOMHET_BEHANDLEARBEIDOGAKTIVITETOPPGAVE_V1_ENDPOINTURL"),
-    val dagpengerArenaHentSakerUrl: String = getEnvVar("DAGPENGER_ARENA_HENTSAKER_URL", "PLACEHOLDER"),
-    val dagpengerInngaaendeJournalUrl: String = getEnvVar("BEHANDLEINNGAAENDEJOURNAL_V1_ENDPOINTURL"),
+    val dagpengerArenaHentSakerUrl: String = getEnvVar("DAGPENGER_ARENA_HENTSAKER_URL",
+            "PLACEHOLDER"),
+    val dagpengerInngaaendeJournalUrl: String = getEnvVar("BEHANDLEINNGAAENDEJOURNAL_V1_ENDPOINTURL",
+            "PLACEHOLDER"),
     val fasitEnvironmentName: String = getEnvVar("FASIT_ENVIRONMENT_NAME", "").filterNot { it in "p" }, //filter out productiony
     val httpPort: Int = 8080
 )
