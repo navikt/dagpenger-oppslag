a6b5eceafdab4cec0151035697507392b0be81d6
Christopher Kolstad <christopher.kolstad@nav.no>
2019-06-18 14:35:20 +0200

    Vi får /api delen i baseUrl fra fasit

--- src/main/kotlin/no/nav/dagpenger/oppslag/ws/aktor/AktorRegisterHttpClient.kt
+++ src/main/kotlin/no/nav/dagpenger/oppslag/ws/aktor/AktorRegisterHttpClient.kt
@@ -14,30 +14,34 @@ class AktorRegisterHttpClient(
 ) {
 
     private val jsonResponseAdapter: JsonAdapter<Map<String, IdentResponse>> = moshiInstance.adapter(
-            Types.newParameterizedType(Map::class.java, String::class.java, IdentResponse::class.java)
+        Types.newParameterizedType(Map::class.java, String::class.java, IdentResponse::class.java)
     )
 
     private fun gjeldendeIdenter(ident: String): List<Ident> {
-        val url = "$baseUrl/api/v1/identer?gjeldende=true"
+        val url = "$baseUrl/v1/identer?gjeldende=true"
 
-        val (_, _, result) = with(url.httpGet()
-                .authentication().bearer(oidcClient.oidcToken().access_token)
-                .header(mapOf(
+        val (_, _, result) = with(
+            url.httpGet().authentication()
+                .bearer(oidcClient.oidcToken().access_token)
+                .header(
+                    mapOf(
                         "Accept" to "application/json",
                         "Nav-Call-Id" to "dagpenger",
                         "Nav-Consumer-Id" to "dagpenger-oppslag",
                         "Nav-Personidenter" to ident
-                ))) {
+                    )
+                )
+        ) {
             responseObject(moshiDeserializerOf(jsonResponseAdapter))
         }
         return result.fold(
-                { success -> success[ident]?.identer ?: emptyList() },
-                { error ->
-                    throw AktorregisterHttpClientException(
-                            error.response.statusCode,
-                            "Failed to fetch name. Response message: ${error.response.responseMessage}. Problem message: ${error.message}"
-                    )
-                }
+            { success -> success[ident]?.identer ?: emptyList() },
+            { error ->
+                throw AktorregisterHttpClientException(
+                    error.response.statusCode,
+                    "Failed to fetch name. Response message: ${error.response.responseMessage}. Problem message: ${error.message}"
+                )
+            }
         )
     }
 
